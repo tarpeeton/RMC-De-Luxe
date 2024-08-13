cf983914c16b95ce37be0af16c9b284418e0c1ef
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-08-13 20:27:45 +0500

    New Coponent UI +

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,7 +9,8 @@ import ChooseUs from './componets/screen/ChooseUS/choose'
 import Buildings from './componets/screen/Buildings/buildings'
 import MortgageCalc from './componets/screen/Mortgage/mortrage'
 import FormCTA from './componets/screen/FormCta/foormCta'
-import Blog from './componets/screen/Blog/Blog'
+import Blog from './componets/screen/Blog/blog'
+import Footer from './componets/screen/Footer/footer'
 
 
 const Home: FC = () => {
@@ -27,6 +28,7 @@ const Home: FC = () => {
         <MortgageCalc />
         <FormCTA />
         <Blog/>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
--- src/componets/screen/Mortgage/mortrage.tsx
+++ src/componets/screen/Mortgage/mortrage.tsx
@@ -10,32 +10,24 @@ const MortgageCalc: FC = () => {
 				<div className={styles.result}>
 					<div className={styles.inputs}>
 						<form className={styles.form}>
-
 							<div className={styles.input__item}>
 								<label className={styles.label}>Стоимость недвижимости (у.е.)</label>
 								<input className={styles.input} placeholder='Введите стоимость' />
 							</div>
-
-
 							<div className={styles.input__item}>
 								<label className={styles.label}>Первоначальный взнос (у.е.)</label>
 								<input className={styles.input} placeholder='Введите размер взноса' />
 							</div>
-
 							<div className={styles.input__item}>	<label className={styles.label}>Срок в годах</label>
 								<input className={styles.input} placeholder='Введите срок' />
 							</div>
-
 							<div className={styles.input__item}>
 								<label className={styles.label}>Ставка (%)</label>
 								<input className={styles.input} placeholder='Введите размер ставки' />
-
 							</div>
-
-
 						</form>
 					</div>
-					<div className={styles.seperator}/>
+					<div className={styles.seperator} />
 					<div className={styles.info}>
 						<div className={styles.info__item}>
 							<p className={styles.info__item__title}>Ежемесячный платеж</p>
